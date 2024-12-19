1a84a5612c2cdb4601db67056ebfbc0d47f3f6ea
Santi7870 <santiago.panchi@udla.edu.ec>
2024-12-18 19:45:27 -0500

    commit7

---
+++ SantiagoPanchi/ViewModels/SPAboutViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SantiagoPanchi.ViewModels
+{
+    internal class SPAboutViewModel
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://www.linkedin.com/in/santiago-panchi-3627ab33b/";
+        public string Message => "¡Hola! Soy Santiago Panchi, un apasionado desarrollador de Ecuador. Me especializo en la creación de aplicaciones móviles y soluciones de software. Mi objetivo es crear aplicaciones que mejoren la vida de las personas y hagan que la tecnología sea más accesible.";
+        public ICommand ShowMoreInfoCommand { get; }
+
+        public SPAboutViewModel()
+        {
+            ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+        }
+
+        async Task ShowMoreInfo() =>
+            await Launcher.Default.OpenAsync(MoreInfoUrl);
+    }
+}
---
+++ SantiagoPanchi/ViewModels/SPNotesViewModel.cs
@@ -0,0 +1,64 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SantiagoPanchi.ViewModels
+{
+    internal class SPNotesViewModel : IQueryAttributable
+    {
+        public ObservableCollection<ViewModels.SPNoteViewModel> AllNotes { get; }
+        public ICommand NewCommand { get; }
+        public ICommand SelectNoteCommand { get; }
+
+        public SPNotesViewModel()
+        {
+            AllNotes = new ObservableCollection<ViewModels.SPNoteViewModel>(Models.Note.LoadAll().Select(n => new SPNoteViewModel(n)));
+            NewCommand = new AsyncRelayCommand(NewNoteAsync);
+            SelectNoteCommand = new AsyncRelayCommand<ViewModels.SPNoteViewModel>(SelectNoteAsync);
+        }
+
+        private async Task NewNoteAsync()
+        {
+            await Shell.Current.GoToAsync(nameof(Views.NotePage));
+        }
+
+        private async Task SelectNoteAsync(ViewModels.SPNoteViewModel note)
+        {
+            if (note != null)
+                await Shell.Current.GoToAsync($"{nameof(Views.NotePage)}?load={note.Identifier}");
+        }
+
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("deleted"))
+            {
+                string noteId = query["deleted"].ToString();
+                SPNoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note exists, delete it
+                if (matchedNote != null)
+                    AllNotes.Remove(matchedNote);
+            }
+            else if (query.ContainsKey("saved"))
+            {
+                string noteId = query["saved"].ToString();
+                SPNoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note is found, update it
+                if (matchedNote != null)
+                {
+                    matchedNote.Reload();
+                    AllNotes.Move(AllNotes.IndexOf(matchedNote), 0);
+                }
+                // If note isn't found, it's new; add it.
+                else
+                    AllNotes.Insert(0, new SPNoteViewModel(Models.Note.Load(noteId)));
+            }
+        }
+    }
+}
